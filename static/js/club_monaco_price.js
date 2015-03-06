56a5eae66b3c3aa49282cce0cc9c50f18c5884ba
daisys <daisyjnshih@gmail.com>
2015-03-06 12:23:34 -0800

    sort data by name and date accordingly, add svg tag to explore

--- static/js/club_monaco_price.js
+++ static/js/club_monaco_price.js
@@ -4,10 +4,61 @@ var x = d3.scale.linear()
     .domain([0, d3.max(data)])
     .range([0, 420]);
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+var products;
+d3.csv("../data/data.csv", function(data){
+    data.sort(function(a, b){
 
+        var cmp1 = d3.ascending(a.Name, b.Name);
+        if (cmp1 != 0){
+            return cmp1;
+        }
+        return d3.ascending(a.Date, b.Date);})
+
+    products = data;
+
+    d3.select('.chart')
+      .selectAll("div")
+      .data(data)
+      .enter()
+      .append("div")
+      .style("width", function(d) {
+            var priceDouble = parseFloat((d.Price).substring(1,(d.Price).length));
+            return priceDouble * 0.1 + "vw"; })
+      .text(function(d,i){
+            return d.Name + " " + d.Date + " " + d.Price;
+      })
+});
+
+var svg = d3.select('.chart')
+            .append('svg')
+            .attr('width', 1000)
+            .attr('height', 400);
+svg.selectAll('circle')
+    .data(data)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d){
+        return d*10;
+    })
+    .attr('cy', function(d){
+        return d*10;
+    })
+    .attr('r', function(d){
+        return 3;
+    })
+    .attr('fill', grey);
+
+svg.selectAll('text')
+    .data(data)
+    .enter()
+    .append('text')
+    .text(function(d){
+       return d;
+       })
+    .attr('x', function(d){
+        return d*10;
+    })
+    .attr('y', function (d){
+        return d;
+    })
+console.log(products);
