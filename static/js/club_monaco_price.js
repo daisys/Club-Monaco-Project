d95fab1b7acbda2821680bb43fdd11d9d9d2b7a9
daisys <daisyjnshih@gmail.com>
2015-03-16 15:32:34 -0700

    added initial dot graph

--- static/js/club_monaco_price.js
+++ static/js/club_monaco_price.js
@@ -33,32 +33,82 @@ var svg = d3.select('.chart')
             .append('svg')
             .attr('width', 1000)
             .attr('height', 400);
-svg.selectAll('circle')
-    .data(data)
-    .enter()
-    .append('circle')
-    .attr('cx', function(d){
-        return d*10;
-    })
-    .attr('cy', function(d){
-        return d*10;
-    })
-    .attr('r', function(d){
-        return 3;
-    })
-    .attr('fill', grey);
 
-svg.selectAll('text')
-    .data(data)
-    .enter()
-    .append('text')
-    .text(function(d){
-       return d;
-       })
-    .attr('x', function(d){
-        return d*10;
-    })
-    .attr('y', function (d){
-        return d;
-    })
-console.log(products);
+d3.csv("../data/data.csv", function(data){
+    data.sort(function(a, b){
+
+        var cmp1 = d3.ascending(a.Name, b.Name);
+        if (cmp1 != 0){
+            return cmp1;
+        }
+        return d3.ascending(a.Date, b.Date);})
+
+    products = data;
+
+    svg.selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('cx', function(d,i){
+            console.log(i);
+            return  i*30;
+
+        })
+        .attr('cy', function(d){
+            var priceDouble = parseFloat((d.Price).substring(1,(d.Price).length));
+                        return priceDouble * 0.1;
+        })
+        .attr('r', function(d){
+            return 2;
+        })
+        .attr('fill', grey)
+        .text(function(d,i){
+                    return d.Name + " " + d.Date + " " + d.Price;
+        });
+
+    svg.selectAll('text')
+        .data(data)
+        .enter()
+        .append('text')
+        .text(function(d){
+           return d;
+           })
+        .attr('x', function(d){
+            return d*10;
+        })
+        .attr('y', function (d){
+            return d;
+        })
+
+
+});
+
+//svg.selectAll('circle')
+//    .data(data)
+//    .enter()
+//    .append('circle')
+//    .attr('cx', function(d){
+//        return d*10;
+//    })
+//    .attr('cy', function(d){
+//        return d*10;
+//    })
+//    .attr('r', function(d){
+//        return 3;
+//    })
+//    .attr('fill', grey);
+//
+//svg.selectAll('text')
+//    .data(data)
+//    .enter()
+//    .append('text')
+//    .text(function(d){
+//       return d;
+//       })
+//    .attr('x', function(d){
+//        return d*10;
+//    })
+//    .attr('y', function (d){
+//        return d;
+//    })
+//console.log(products);
